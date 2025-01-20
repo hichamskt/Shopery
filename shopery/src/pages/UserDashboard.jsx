2b88d3ce592b6a70955607394cdf27d887c9da55
unknown <hichamsskouti@gmail.com>
2025-01-20 20:07:42 +0100

    profile

--- shopery/src/pages/UserDashboard.jsx
+++ shopery/src/pages/UserDashboard.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import '../styles/UserDashboard.css'
+import noProfil from "../assets/no_profile.png"
 
 function UserDashboard() {
   return (
-    <div>UserDashboard</div>
+    <div>
+      <div>
+        <ProfileBox />
+        <UserInfoBox />
+      </div>
+
+    </div>
   )
 }
 
 export default UserDashboard
+
+
+
+
+
+function ProfileBox (){
+
+
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+
+function UserInfoBox(){
+
+  return(
+    <div>
+
+    </div>
+  )
+}
