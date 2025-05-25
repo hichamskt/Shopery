4bc8006641d823326a0d1bec588f1c91984ac1ab
unknown <hichamsskouti@gmail.com>
2025-05-25 23:24:20 +0100

    user profile setting

--- shopery/src/pages/Settings.jsx
+++ shopery/src/pages/Settings.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/Settings.css'
-import useAuth from '../hooks/useAuth';
-import { axiosPrivate } from '../axios/axiosInstance';
-import BillingInput from '../UI/BillingInput/BillingInput';
-
-
+import React, { useEffect, useRef, useState } from "react";
+import "../styles/Settings.css";
+import useAuth from "../hooks/useAuth";
+import axiosInstance, { axiosPrivate } from "../axios/axiosInstance";
+import BillingInput from "../UI/BillingInput/BillingInput";
+import profile from "../assets/no_profile.png"
 
 function Settings() {
-
-const [userInfo, setUserInfo] = useState([]);
+  const [userInfo, setUserInfo] = useState([]);
   const { auth } = useAuth();
   useEffect(() => {
     const fetchData = async () => {
@@ -28,142 +26,211 @@ const [userInfo, setUserInfo] = useState([]);
     fetchData();
   }, []);
 
-console.log('settingdata:',userInfo)
+  console.log("settingdata:", userInfo);
+  console.log("settingdataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
   return (
     <div>
-        <AccountSettings userInfo={userInfo} setUserInfo={setUserInfo} />
-
+      <AccountSettings userInfo={userInfo} setUserInfo={setUserInfo} auth={auth} />
     </div>
-  )
+  );
 }
 
-export default Settings
+export default Settings;
+
+function AccountSettings({ userInfo, setUserInfo , auth}) {
+  const [errors, setErrors] = useState({});
+  const fileInputRef = useRef(null);
+  
+  const inputs1 = [
+    {
+      id: 1,
+      name: "firstName",
+      type: "text",
+      placeholder: "Your first name",
+      errorMessage:
+        "First name  must only contain letters and be between 2 and 15 characters long",
+      label: "First name",
+      pattern: "^[A-Za-z]{2,15}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "lastName",
+      type: "text",
+      placeholder: "Your Last Name",
+      errorMessage:
+        "The Last name must only contain letters and be between 2 and 15 characters long",
+      label: "Last Name",
+      pattern: "^[A-Za-z]{2,15}$",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 4,
+      name: "phoneNumber",
+      type: "text",
+      placeholder: "Phone Number",
+      errorMessage: "It should be a valid Phone Number!",
+      label: "Phone Number",
+      pattern: "[0-9]{9,11}",
+      required: true,
+    },
+  ];
+
+  const handleValueError = () => {
+    const newErrors = {};
+    if (!userInfo.email.trim()) {
+      newErrors.email = "Email is required.";
+    } else if (!/^\S+@\S+\.\S+$/.test(userInfo.email)) {
+      newErrors.email = "Enter a valid email address.";
+    }
+
+    if (!userInfo.phoneNumber.trim()) {
+      newErrors.phoneNumber = "Phone Number is required.";
+    } else if (!/^[0-9]{9,11}$/.test(userInfo.phoneNumber)) {
+      newErrors.phoneNumber = "It should be a valid Phone Number!";
+    }
+
+    if (!userInfo.firstName.trim()) {
+      newErrors.firstName = "First Name Is required";
+    } else if (!/^[A-Za-z]{2,15}$/.test(userInfo.firstName)) {
+      newErrors.firstName =
+        "First name  must only contain letters and be between 2 and 15 characters long";
+    }
+    if (!userInfo.lastName.trim()) {
+      newErrors.lastName = "Last Name Is required";
+    } else if (!/^[A-Za-z]{2,15}$/.test(userInfo.lastName)) {
+      newErrors.lastName =
+        "Last name  must only contain letters and be between 2 and 15 characters long";
+    }
+    return newErrors;
+  };
 
 
+  console.log(userInfo.images)
+  const submitForm = async()=>{
 
-function AccountSettings ({userInfo,setUserInfo}){
-
-const [errors,setErrors]=useState({});
-    const inputs1 = [
-        {
-            id: 1,
-            name: "firstName",
-            type: "text",
-            placeholder: "Your first name",
-            errorMessage:
-              "First name  must only contain letters and be between 2 and 15 characters long",
-            label: "First name",
-            pattern: "^[A-Za-z]{2,15}$",
-            required: true,
-          },
-          {
-            id: 2,
-            name: "lastName",
-            type: "text",
-            placeholder: "Your Last Name",
-            errorMessage:
-              "The Last name must only contain letters and be between 2 and 15 characters long",
-            label: "Last Name",
-            pattern: "^[A-Za-z]{2,15}$",
-            required: true,
-          },
-        {
-            
-          id: 3,
-          name: "email",
-          type: "email",
-          placeholder: "Email",
-          errorMessage: "It should be a valid email address!",
-          label: "Email",
-          required: true,
-        },
-        {
-          id: 4,
-          name: "phoneNumber",
-          type: "text",
-          placeholder: "Phone Number",
-          errorMessage: "It should be a valid Phone Number!",
-          label: "Phone Number",
-          pattern: "[0-9]{9,11}",
-          required: true,
-        },
-      ];
+    const formData = new FormData();
+    formData.append("email", auth.email);
+    formData.append("updatedemail", userInfo.email);
+    formData.append("lastName", userInfo.lastName);
+    formData.append("firstName", userInfo.firstName);
+    formData.append("phoneNumber", userInfo.phoneNumber);
+    formData.append("images", userInfo.images.file);
 
 
-      const handleValueError = () => {
-        const newErrors = {};
-        if (!userInfo.Email.trim()) {
-          newErrors.Email = "Email is required.";
-        } else if (!/^\S+@\S+\.\S+$/.test(userInfo.Email)) {
-          newErrors.billingEmail = "Enter a valid email address.";
-        }
-       
     
-        if (!userInfo.phoneNumber.trim()) {
-          newErrors.phoneNumber = "Phone Number is required.";
-        } else if (!/^[0-9]{9,11}$/.test(userInfo.phoneNumber)) {
-          newErrors.phoneNumber = "It should be a valid Phone Number!";
-        }
+    try {
+      const response = await axiosInstance.post("user/updateAcountSettings", formData,
+       {
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+      );
+
+      if (response.status === 201) {
+        console.log("updated")
+
+      }
+    } catch (err) {
+      console.log("Error fetching data:", err);
+    }
+
+  }
+
+  const handleSubmitOrderForm = () => {
+    const newErrors = handleValueError();
+    setErrors(newErrors);
+    console.log("clicked")
     
+    if (Object.keys(newErrors).length === 0) {
+      submitForm();
+      console.log("submitted")
+    }
+  };
+
+
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo({
+      ...userInfo,
+      [name]: value,
+    });
+
+    if (errors[name]) {
+      const updatedErrors = { ...errors };
+
+      delete updatedErrors[name];
+      setErrors(updatedErrors);
+    }
+  };
+
+  function onFileSelect(event) {
+    const file = event.target.files[0];
+
+    if (file.length === 0) return;
+
     
-        if (!userInfo.FirstName.trim()) {
-          newErrors.FirstName = "First Name Is required";
-        } else if (!/^[A-Za-z]{2,15}$/.test(userInfo.FirstName)) {
-          newErrors.FirstName =
-            "First name  must only contain letters and be between 2 and 15 characters long";
-        }
-        if (!userInfo.LastName.trim()) {
-          newErrors.LastName = "Last Name Is required";
-        } else if (!/^[A-Za-z]{2,15}$/.test(userInfo.LastName)) {
-          newErrors.LastName =
-            "Last name  must only contain letters and be between 2 and 15 characters long";
-        }
-        return newErrors;
-      };
+    setUserInfo((prevUserInfo) => ({
+      ...prevUserInfo,
+      images: {
+        file: file,
+        name: file.name,
+        url: URL.createObjectURL(file),
+      },
+    }));
 
-
-
-      const onChange = (e) => {
-        
-        const { name, value,  } = e.target;
-        setUserInfo({
-            ...userInfo,
-            [name] : value,
-          });
-       
-        if (errors[name]) {
-          const updatedErrors = { ...errors };
     
-          delete updatedErrors[name];
-          setErrors(updatedErrors);
-        }
-      };
+  }
+  function selectFile() {
+    fileInputRef.current.click();
+  }
 
+  console.log(typeof userInfo.images === "object")
+  
 
+  return (
+    <div className="acsetting">
+      <div className="acsetheader">Account Settings</div>
+      <div className="acsetbox">
+        <div>
+          {inputs1.map((input) => (
+            <BillingInput
+              key={input.id}
+              {...input}
+              value={userInfo[input.name]}
+              onChange={onChange}
+              err={errors[input.name]}
+            />
+          ))}
 
-
-
-    return<div className='acsetting'>
-        <div className='acsetheader'>
-        Account Settings
+          <button className="savesett" onClick={()=>handleSubmitOrderForm()}>Save Changes</button>
         </div>
-        <div className='acsetbox'>
-            <div>
-            {inputs1.map((input) => (
-          <BillingInput
-            key={input.id}
-            {...input}
-            value={userInfo[input.name]}
-            onChange={onChange}
-            err={errors[input.name]}
-          />
-        ))}
-        
-        <button className='savesett'>Save Changes</button>
-            </div>
-            <div></div>
+        <div className="profilesetting">
+          <img 
+           src={
+            userInfo.images?
+            typeof userInfo.images === "object"
+            ? userInfo.images.url:
+          `${process.env.REACT_APP_BACKEND_URL}${userInfo.images}`
+          : profile
+          }
+          
+          
+          alt="profile"/>
+          <input type="file" style={{display:"none"}} name="images"  onChange={onFileSelect} ref={fileInputRef}/>
+          <button onClick={selectFile}>Chose image</button>
         </div>
-
+      </div>
     </div>
+  );
 }
